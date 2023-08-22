aabfeb75c1768e5e77692b3b5ea85b7e2a5cbda4
bilyashik <138653003+bilyashik@users.noreply.github.com>
2023-08-22 11:45:03 +0600

    homework-4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function RegistrationForm() {
+  const [formData, setFormData] = useState({
+    fullName: '',
+    phoneNumber: '',
+    email: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="registration-form">
+      <h2>Регистрация пользователя</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="fullName">ФИО:</label>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phoneNumber">Номер телефона:</label>
+          <input
+            type="tel"
+            id="phoneNumber"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email адрес:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </div>
+      </form>
+      <div className="preview">
+        <h3>Ваши данные:</h3>
+        <p>ФИО: {formData.fullName}</p>
+        <p>Номер телефона: {formData.phoneNumber}</p>
+        <p>Email адрес: {formData.email}</p>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default RegistrationForm;
